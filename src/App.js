0108282db7b99fdbcfc65434e67155c27727d450
Jared Palmer <jared@palmer.net>
2018-10-17 16:03:59 -0400

    init covfefe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+import { Router } from '@reach/router';
+import { fetch, setToken } from './fetch';
+import Search from './Search';
+import Artist from './Artist/Artist';
+class Auth extends React.Component {
+  componentDidMount() {
+    let hashParams = {};
+    let e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    if (hashParams.access_token) {
+      setToken(hashParams.access_token);
+      window.location.href = '/';
+    }
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+    return null;
+  }
+}
+class App extends React.Component {
+  state = {};
+
+  componentDidMount() {
+    this.getMe();
+  }
+
+  getMe = () => {
+    fetch('https://api.spotify.com/v1/me')
+      .then(res => res.json())
+      .then(
+        user => console.log(user) || this.setState({ user }),
+        error => console.log(error)
+      );
+  };
+
+  render() {
+    console.log(this.state);
+
+    if (this.state.user === null) {
+      return (
+        <div className="login">
+          <a href="https://accounts.spotify.com/authorize?client_id=7baf7bed653c410bbdc7966a1dd4e0b2&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback/">
+            Login with Spotify
           </a>
-        </header>
+        </div>
+      );
+    }
+
+    return (
+      <div className="app">
+        <div className="nav">
+          <div className="logo">
+            <span role="img" aria-label="piano">
+              🎹
+            </span>
+            Spotify
+          </div>
+        </div>
+        <Router>
+          <Auth path="/callback" />
+          <Search path="/" />
+          <Artist path="/a/:id" />
+        </Router>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { unstable_createRoot as createRoot } from 'react-dom';
+import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+root.render(<App />);
