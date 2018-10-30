5e2cabb18faebaec0db5553cb92ebaeb9a3c91a9
Jared Palmer <jared@palmer.net>
2018-10-30 11:35:26 -0400

    Remove node_modules from before and conf

--- before-suspense/node_modules/the-platform/dist/utils.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var isBrowser = exports.isBrowser = typeof window !== 'undefined';
--- conf-talk-progress/node_modules/react-error-boundary/dist/es/index.js
+++
@@ -1,6 +0,0 @@
-import ErrorBoundaryFallbackComponent from './ErrorBoundaryFallbackComponent';
-import ErrorBoundary, { withErrorBoundary } from './ErrorBoundary';
-
-export default ErrorBoundary;
-
-export { ErrorBoundary, withErrorBoundary, ErrorBoundaryFallbackComponent };
